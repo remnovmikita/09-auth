818d1380e11ac2355f2950e632bfaaab35bafda1
Nikita <remnovmikita@gmail.com>
2026-05-03 15:39:31 +0300

    delete client notfound

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 
 import css from './page.module.css';
 import Link from 'next/link';
-import RedirectCountdown from '@/components/Not-Found/Not-found.clien';
+// import RedirectCountdown from '@/components/Not-Found/Not-found.client';
 
 export default function NotFound() {
   return (
@@ -9,7 +9,7 @@ export default function NotFound() {
       <h1 className={css.title}>404 - Page not found</h1>
       <p className={css.desc}>Sorry, the page you are looking for does not exist.</p>
       <Link href="/" className={css.link}>Go back home</Link>
-      <RedirectCountdown />
+      {/* <RedirectCountdown /> */}
     </div>
   );
 }
